2dafbb3ef93ae1565a74434ef70f79546b6199d2
Rafał Sztandera <rafszt@gmail.com>
2017-02-09 22:06:00 +0100

    footer proto; local adjustments

--- demo/templatetags/demo_tags.py
+++ demo/templatetags/demo_tags.py
@@ -45,6 +45,27 @@ def top_menu(context, parent, calling_page=None):
     }
 
 
+# Retrieves the top menu items - the immediate children of the parent page
+# The has_menu_children method is necessary because the bootstrap menu requires
+# a dropdown class to be applied to a parent
+@register.inclusion_tag('demo/tags/plain_menu.html', takes_context=True)
+def plain_menu(context, parent, calling_page=None):
+    menuitems = parent.get_children().live().in_menu()
+    for menuitem in menuitems:
+        menuitem.show_dropdown = has_menu_children(menuitem)
+        # We don't directly check if calling_page is None since the template
+        # engine can pass an empty string to calling_page
+        # if the variable passed as calling_page does not exist.
+        menuitem.active = (calling_page.url.startswith(menuitem.url)
+                           if calling_page else False)
+    return {
+        'calling_page': calling_page,
+        'menuitems': menuitems,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
+
+
 # Retrieves the children of the top menu items for the drop downs
 @register.inclusion_tag('demo/tags/top_menu_children.html', takes_context=True)
 def top_menu_children(context, parent):
